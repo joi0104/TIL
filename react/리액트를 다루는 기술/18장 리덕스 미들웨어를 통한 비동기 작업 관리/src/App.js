1fea2b43595a68d4c730a94fe8757855e9d30aad
joi0104 <jjjjoi0104@gmail.com>
2020-12-18 22:58:15 +0900

    2020.12.18

---
+++ react/리액트를 다루는 기술/18장 리덕스 미들웨어를 통한 비동기 작업 관리/src/App.js
@@ -0,0 +1,13 @@
+import CounterContainer from "./containers/CounterContainer";
+import SampleContainer from "./containers/SampleContainer";
+
+function App() {
+  return (
+    <>
+      <CounterContainer />
+      <SampleContainer />
+    </>
+  );
+}
+
+export default App;
